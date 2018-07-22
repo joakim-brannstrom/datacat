ae94722f2f2c2391a6787c9da683d935c9ddc6a1
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-07-22 20:01:23 +0200

    travis: build standalone

--- travis.sh
+++ travis.sh
@@ -7,4 +7,10 @@ dub build
 pushd test
 # checking that the perf suite can build
 dub build -c unittest -b unittest
+
+pushd standalone
+# test building the standalone apps
+dub build -c graspan1 -b release
+popd
+
 popd
